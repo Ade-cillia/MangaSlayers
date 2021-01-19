bfc67dae6ad41703577370c2caebae411c62e0f4
BACETTI <kenzibacetti@gmail.com>
2021-01-19 17:31:04 +0100

    css revu un peu

---
+++ controller/controller_homealg.php
@@ -0,0 +1,3 @@
+<?php 
+include './model/homealg.php';
+?>
--- view/view_home.php
+++ view/view_home.php
@@ -8,10 +8,10 @@
     <title>Accueil</title>
 </head>
 <body>
-<div class="chercher">
+<div class="chercher floatLeft positionAbsolute">
     <div class="Cat1">
         <button class="CatB">Catégories</button>
-        <div class="Cat2">
+        <div class="Cat2 positionAbsolute ">
         <a href="category?id_category=1">Horreur</a>
         <a href="category?id_category=2">Aventure</a>
         <a href="category?id_category=3">Heroic-Fantasy</a>
@@ -28,39 +28,41 @@
 	</button>
 </form>
 </div>
-<div class="selectedforyou">
-<h1>Selectionnés pour vous</h1>
+<div class="titletitle">
+        <h1>Selectionnés pour vous</h1>
 </div>
-<div class="generalhome floatRight floatLeft positionAbsolute Aurelpascontent">
-    <?php
-    foreach ($itemforyou as $itemsforyou) {
-        //echo($itemsforyou);
-        foreach (getItem($pdo,-1,$itemsforyou) as $item) {
-            echo "<div class='blockItemhome'>";
-            echo "  <div class='left'>";
-            echo "      <img class='image' src='".$item["image"]."'>";
-            echo "  </div>";
-            echo "  <div class='right'>";
-            echo "      <div class='nameItem'>";
-            echo "          <h3><strong>Titre : &nbsp&nbsp</strong>".$item['name']."</h3>";
-            echo "          <div><strong>Autheur : </strong>".$item['authors']."</p></div>";
-            echo "          <div><strong>Prix : </strong>".$item['price']."€</p></div>";
-            echo "      </div>";
-            echo "      <div class='Description'>";
-            echo "          <strong>Description: </strong>".tronque_description($item['description'], 250);
-            echo "      </div>";
-            echo "      <div class='buyButtonDiv'>";
-            echo "          <form action='home?id_manga_title=".$itemsforyou."&id_item=".$item['id']."&buy_item=1&home_buy=true' method='post'>";
-            echo "              <button id='buyButton' type='submit' name='buyButton'><h1>Ajouter au panier</h1></button>";
-            echo "          </form>";
-            echo "      </div>";
-            echo "  </div>";
-            echo "</div>";
-        }
-    }   
-    ?>
+<div class="fleex">
+    <div class="generalhome">
+        <?php
+        foreach ($itemforyou as $itemsforyou) {
+            //echo($itemsforyou);
+            foreach (getItem($pdo,-1,$itemsforyou) as $item) {
+                echo "<div class='blockItemhome'>";
+                echo "  <div class='left'>";
+                echo "      <img class='image' src='".$item["image"]."'>";
+                echo "  </div>";
+                echo "  <div class='right'>";
+                echo "      <div class='nameItem'>";
+                echo "          <h3><strong>Titre : &nbsp&nbsp</strong>".$item['name']."</h3>";
+                echo "          <div><strong>Autheur : </strong>".$item['authors']."</p></div>";
+                echo "          <div><strong>Prix : </strong>".$item['price']."€</p></div>";
+                echo "      </div>";
+                echo "      <div class='Description'>";
+                echo "          <strong>Description: </strong>".tronque_description($item['description'], 250);
+                echo "      </div>";
+                echo "      <div class='buyButtonDiv'>";
+                echo "          <form action='home?id_manga_title=".$itemsforyou."&id_item=".$item['id']."&buy_item=1&home_buy=true' method='post'>";
+                echo "              <button id='buyButton' type='submit' name='buyButton'><h1>Ajouter au panier</h1></button>";
+                echo "          </form>";
+                echo "      </div>";
+                echo "  </div>";
+                echo "</div>";
+            }
+        }   
+        ?>
+    </div>
 </div>
-<div class="meilleursventes">
+<div class="titletitle">
 <h1>Meilleurs Ventes</h1>
 </div>
 </body>
